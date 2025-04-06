fbd818f5e56e394334fedccdc47afc53a59cdcdb
Samusain <samzy334@gmail.com>
2025-04-06 12:25:29 +0100

    gh-pages installed and index.js file edited

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider} from 'react-router-dom';
-import Error from './Components/Error/Error'
 import App from './App';
-
-
-const router1 = createBrowserRouter([
-  {
-    path: "/baseapparel",
-    element: <App/>
-  },
-  {
-    path: "/*",
-    element: <Error/>
-  }
-])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router1} />
+    <App />
   </React.StrictMode>
-)
-
+);
